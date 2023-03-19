6e3ba3a1684945361ec2b395fe95efbc649ebe05
Marco Useche <usechermarcoj@gmail.com>
2023-03-19 16:00:27 -0500

    add logs

--- src/auth/serializer/session.serializer.ts
+++ src/auth/serializer/session.serializer.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PassportSerializer } from '@nestjs/passport';
 
@@ -8,6 +8,7 @@ import { User } from '../entities/user.entity';
 
 @Injectable()
 export class SessionSerializer extends PassportSerializer {
+  private logger = new Logger(SessionSerializer.name);
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
@@ -16,14 +17,17 @@ export class SessionSerializer extends PassportSerializer {
   }
 
   async serializeUser(user: User, done: CallableFunction) {
+    this.logger.log('serializeUser', JSON.stringify(user))
     done(null, user.id);
   }
 
   async deserializeUser(id: number, done: CallableFunction) {
     const user = await this.userRepository.findOneBy({ id });
+    this.logger.log('deserializeUser', JSON.stringify(user), id)
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { password, createdAt, updatedAt, ...newUser } = user;
 
+    this.logger.log('deserializeUser', JSON.stringify(newUser));
     return newUser ? done(null, newUser) : done(null, null);
   }
 }
